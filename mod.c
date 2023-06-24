f6c6fb88218402f3dc27a8579d58da3bcc7c2be2
laamyry <slamiri10@gmail.com>
2023-06-24 14:51:37 +0100

    monty project

--- mod.c
+++ mod.c
@@ -1,30 +1,30 @@
 #include "monty.h"
-
 /**
- * mo_mod - computes the modulus of the second top element of the stack
- *          with the top element of the stack.
- * @stack: pointer to the top of the stack
- * @line_num: line number of the opcode
+ * mo_mod -  computes the rest of the division of the second
+ * top element of the stack by the top element of the stack.
+ *
+ * @stack: Pointer.
+ * @line_num: Line number.
  */
 void mo_mod(stack_t **stack, unsigned int line_num)
 {
-	int a, b;
+    int m, n;
 
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_num);
-		exit(EXIT_FAILURE);
-	}
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't mod, stack too short\n", line_num);
+        exit(EXIT_FAILURE);
+    }
 
-	a = (*stack)->n;
-	b = (*stack)->next->n;
+    m = (*stack)->n;
+    n = (*stack)->next->n;
 
-	if (a == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", line_num);
-		exit(EXIT_FAILURE);
-	}
+    if (m == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", line_num);
+        exit(EXIT_FAILURE);
+    }
 
-	(*stack)->next->n = b % a;
-	mo_pop(stack, line_num);
+    (*stack)->next->n = n % m;
+    mo_pop(stack, line_num);
 }
